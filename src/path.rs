8b8e3205191b3eac85c244ac7ce6e2a3db0b0e5b
beltegeuse <adrien.gruson@gmail.com>
2018-04-01 15:43:51 +0900

    continue to write the layout for path mutation.

--- src/path.rs
+++ src/path.rs
@@ -38,7 +38,7 @@ impl<'a> Vertex<'a> {
 
     pub fn generate_next(&mut self,
                          scene: &'a Scene,
-                         sampler: &mut Sampler) -> (Option<Edge>, Option<Vertex<'a>>) {
+                         sampler: Option<&mut Sampler>) -> (Option<Edge>, Option<Vertex<'a>>) {
         match *self {
             Vertex::Sensor(ref mut v) => {
                 let ray = scene.camera.generate(v.uv);
@@ -58,6 +58,9 @@ impl<'a> Vertex<'a> {
                 )
             }
             Vertex::Surface(ref mut v) => {
+                assert!(!sampler.is_none());
+                let sampler = sampler.unwrap();
+
                 v.sampled_bsdf = match v.its.mesh.bsdf.sample(&v.its.wi, sampler.next2d()) {
                     Some(x) => Some(x),
                     None => return (None, None)
@@ -129,7 +132,7 @@ impl<'a> Path<'a> {
 
         let mut depth = 1;
         while max_depth.map_or(true, |max| depth < max) {
-            match vertices.last_mut().unwrap().generate_next(scene, sampler) {
+            match vertices.last_mut().unwrap().generate_next(scene, Some(sampler)) {
                 (Some(edge), Some(vertex)) => {
                     edges.push(edge);
                     vertices.push(vertex);
@@ -161,10 +164,57 @@ impl<'a> Path<'a> {
         })
     }
 
-    pub fn shift_geometric(&self, shift_pix: (f32, f32)) -> Option<Path<'a>> {
+    pub fn shift_geometric(&self, shift_pix: Point2<f32>, scene: &'a Scene) -> Option<Path<'a>> {
         // FIXME: Need to implement G-PT shift mapping
         // FIXME: The idea of this code is to shift the path geometry
         // FIXME: without evaluating the direct lighting (compared to G-PT)
-        None
+        let mut v0 = Vertex::new_sensor_vertex(shift_pix,
+                                               scene.camera.param.pos);
+        let (e0, v1) = match v0.generate_next(scene, None) {
+            (Some(e), Some(v)) => (e,v),
+            _ => return None, // FIXME: This is not correct for now
+        };
+
+        let mut vertices = vec![v0, v1];
+        let mut edges = vec![e0];
+
+        // FIXME: This code will do explicit reconnection
+        for i in 1..self.vertices.len() {
+            match i {
+                1 => {
+                    match self.vertices.get(i + 1) {
+                        None => return Some(Path {
+                            vertices,
+                            edges,
+                        }),
+                        Some(&Vertex::Surface(ref main_next)) => {
+                            let main_edge = &self.edges[i - 1];
+                            let shift_current = match vertices.last().unwrap() {
+                                &Vertex::Surface(ref x) => x,
+                                _ => panic!("Un-expected path for the shift mapping"), // If we have something else, panic!
+                            };
+
+                            let shift_d_out_global = (main_next.its.p - shift_current.its.p).normalize();
+                            let shift_d_out_local = shift_current.its.frame.to_local(shift_d_out_global);
+                            let jacobian = ((main_next.its.n_g.dot(-shift_d_out_global) * main_next.its.dist.powi(2)).abs()
+                                / (main_next.its.n_g.dot(-main_edge.d) * (shift_current.its.p - main_next.its.p).magnitude2()).abs()) as f64;
+
+                            assert!(jacobian.is_finite());
+                            assert!(jacobian >= 0.0);
+
+                            // FIXME: Need to update throughput and pdf
+                        },
+                        _ => panic!("Encounter wrong vertex type"),
+                    }
+                },
+                2 => {},
+                _ => {},
+            }
+        }
+
+        Some(Path {
+            vertices,
+            edges,
+        })
     }
 }
